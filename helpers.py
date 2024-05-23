67640d505b3f809de11e7a73ffe9183d2e00d993
Taylor Price <tayworm@gmail.com>
2024-05-23 15:23:41 -0700

    fix: handle az cli not being installed.. exit with helpful error
    Signed-off-by: Taylor Price <tayworm@gmail.com>

--- helpers.py
+++ helpers.py
@@ -183,9 +183,14 @@ if __name__ == "__main__":
     import asyncio
 
     # az login
-    command = ["az", "login", "--only-show-errors", "-o",
-               "none"]
-    result = subprocess.run(command, stdin=None)
+    try:
+        command = ["az", "login", "--only-show-errors", "-o",
+                   "none"]
+        result = subprocess.run(command, stdin=None)
+    except FileNotFoundError:
+        print("Azure CLI not found. Please install it.", file=sys.stderr)
+        sys.exit(1)
+        
     if result.returncode != 0:
         print("Failed to login to Azure.", file=sys.stderr)
         sys.exit(1)
